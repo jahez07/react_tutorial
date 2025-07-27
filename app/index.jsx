a8c8a4d979c893931c113bcec8372d245c50f2f3
Jahez Abraham Johny <jaj@sureflow.com>
2025-07-27 18:29:33 +0400

    React Router Installation

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+// Represents the home page/entry point of the application.
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
